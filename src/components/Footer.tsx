680bfb66903a879ca905f8ba497e6379c26ca63e
egorpetr <52234539+egorpetr@users.noreply.github.com>
2024-07-20 00:49:05 +0300

    fixed: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,45 +12,41 @@ const Footer: React.FC<FooterProps> = ({
   Sirius,
 }) => {
   return (
-    <footer className="flex justify-center items-center self-stretch px-16 py-12 mt-32 w-full bg-gradient-to-t from-[#1919EF]">
+    <footer className="flex justify-center items-center self-stretch px-5 py-12 mt-32 w-full bg-gradient-to-t from-[#1919EF]">
       <div className="flex flex-col mt-9 w-full max-w-[1026px] max-md:max-w-full">
         <div className="justify-center max-md:max-w-full">
           <div className="flex gap-5 max-md:flex-col max-md:gap-0">
             <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
               <div className="flex flex-col grow justify-center mr-auto text-lg font-medium text-white max-md:mt-10">
-                <img
-                  loading="lazy"
-                  src={logo}
-                  className="self-start aspect-[7.14] w-[296px]"
-                  alt="Logo"
-                />
+                <a href="/">
+                  <img
+                    loading="lazy"
+                    src={logo}
+                    className="self-start aspect-[7.14] w-[296px]"
+                    alt="Logo"
+                  />
+                </a>
                 <div className="mt-3.5 mr-auto">
-                  Федеральная территория «Сириус», Олимпийский пр-т, 40
+                  Федеральная территория «Сириус», <br></br>Олимпийский пр-т, 40
                 </div>
                 <div className="mt-3.5 mr-auto">info@nedvizhka.tech</div>
               </div>
             </div>
-            <div className="flex flex-col ml-5 w-[67%] max-md:ml-0 max-md:w-full">
-              <div className="flex flex-col gap-5 justify-center px-16 py-2 w-full text-lg font-medium text-white bg-white bg-opacity-20 rounded-[100px] max-md:px-5 max-md:mt-10">
-                <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-                  <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
-                    <div className="justify-center py-1">
-                      <a className="text-base  hover:text-blue-500 transition-colors ease-in-out duration-300" href="#evaluation">Перейти к оценке</a>
-                    </div>
-                  </div>
-                  <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
-                    <div className="justify-center py-1">
-                      <a className="text-base  hover:text-blue-500 transition-colors ease-in-out duration-300" href="#how-it-works">Как это работает?</a>
-                    </div>
-                  </div>
-                  <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
-                    <div className="justify-center py-1">
-                      <a className="text-base  hover:text-blue-500 transition-colors ease-in-out duration-300" href="/for-business">Для бизнеса</a>
-                    </div>
-                  </div>
+            <div className="hidden md:block w-[67%]">
+            <div className="flex flex-col gap-5 justify-center px-16 py-5 w-full text-lg font-medium text-white bg-white bg-opacity-20 rounded-[100px] max-md:px-5 max-md:mt-10">
+              <div className="flex justify-between gap-5 max-md:flex-col max-md:gap-0">
+                <div className="w-[30%] text-center">
+                  <a href="#evaluation" className="py-1 block">Перейти к оценке</a>
+                </div>
+                <div className="w-[30%] text-center">
+                  <a href="#how-it-works" className="py-1 block">Как это работает?</a>
+                </div>
+                <div className="w-[30%] text-center">
+                  <a href="/for-business" className="py-1 block">Для бизнеса</a>
                 </div>
               </div>
             </div>
+          </div>
           </div>
         </div>
         <div className="flex gap-5 justify-between mt-24 w-full max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
@@ -65,18 +61,22 @@ const Footer: React.FC<FooterProps> = ({
             </div>
           </div>
           <div className="flex gap-5 justify-center self-end mt-7 ml-auto max-md:flex-wrap max-md:max-w-full">
-            <img
-              loading="lazy"
-              src={GPB}
-              className="self-start mt-3 aspect-[7.14] w-[237px]"
-              alt="GPB"
-            />
-            <img
-              loading="lazy"
-              src={Sirius}
-              className="shrink-0 max-w-full aspect-[4.35] w-[200px]"
-              alt="Sirius"
-            />
+            <a href="https://www.gpbspace.ru">
+              <img
+                loading="lazy"
+                src={GPB}
+                className="self-start mt-3 aspect-[7.14] w-[237px]"
+                alt="GPB"
+              />
+            </a>
+            <a href="https://sochisirius.ru">
+              <img
+                loading="lazy"
+                src={Sirius}
+                className="shrink-0 max-w-full aspect-[4.35] w-[200px]"
+                alt="Sirius"
+              />
+            </a>
           </div>
         </div>
       </div>
